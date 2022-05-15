d5856ace004e8cedda6587a60712f13a19190505
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-15 09:13:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sda_csharp_exercises_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: do some excercises...
+        }
+    }
+}
