f323d4ac517bb133eea8505953ede8584162ddd4
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-21 09:55:56 +0200

    E22 solved abstract class

---
+++ Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sda_csharp_exercises_classes
+{
+    class Circle:AbstractShape
+    {
+        public int Radius { get; set; }
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+        public override double GetArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,15 @@
+﻿namespace sda_csharp_exercises_classes
+{
+    class Square:AbstractShape
+    {
+        public int Side { get; set; }
+        public Square(int side)
+        {
+            Side = side;
+        }
+        public override double GetArea()
+        {
+            return Side * Side;
+        }
+    }
+}
