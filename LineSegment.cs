6a182e09f9c29b12b31f41995dc76a25e1b98ec9
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-21 11:54:10 +0200

    E24 solved

--- LineSegment.cs
+++ LineSegment.cs
@@ -4,21 +4,21 @@ namespace sda_csharp_exercises_classes
 {
     class LineSegment
     {
-        public MyPoint Start { get; set; }
-        public MyPoint End { get; set; }
-        public LineSegment(MyPoint start, MyPoint end)
+        public MyPoint3D Start { get; set; }
+        public MyPoint3D End { get; set; }
+        public LineSegment(MyPoint3D start, MyPoint3D end)
         {
             Start = start;
             End = end;
         }
-        public LineSegment(int startX, int startY, int endX, int endY)
+        public LineSegment(int startX, int startY, int startZ, int endX, int endY, int endZ)
         {
-            Start = new(startX, startY);
-            End = new(endX, endY);
+            Start = new(startX, startY, startZ);
+            End = new(endX, endY, endZ);
         }
         public double Lenght()
         {
-            return Math.Sqrt(Math.Pow(End.X - Start.X, 2) + Math.Pow(End.Y - Start.Y, 2));
+            return Math.Sqrt(Math.Pow(End.X - Start.X, 2) + Math.Pow(End.Y - Start.Y, 2) + Math.Pow(End.Z - Start.Z, 2));
         }
     }
 }
---
+++ MyPoint3D.cs
@@ -0,0 +1,11 @@
+﻿namespace sda_csharp_exercises_classes
+{
+    class MyPoint3D : MyPoint
+    {
+        public int Z { get; set; }
+        public MyPoint3D(int x, int y, int z) : base(x, y)
+        {
+            Z = z;
+        }
+    }
+}
