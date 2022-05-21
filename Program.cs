505cf829f53f06e11c4ef7b8eb54297112760ce1
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-21 11:58:02 +0200

    E24 Solved

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace sda_csharp_exercises_classes
     {
         static void Main(string[] args)
         {
-            MyPoint start = new(1, 2);
-            MyPoint end = new(5, 6);
-            LineSegment lineSegment1 = new(1, 2, 5, 6);
+            MyPoint3D start = new(1, 2,3);
+            MyPoint3D end = new(5, 6,7);
+            LineSegment lineSegment1 = new(1, 2,3, 5, 6,7);
             LineSegment lineSegment2 = new(start, end);
             Console.WriteLine(lineSegment1.Lenght());
             Console.WriteLine(lineSegment2.Lenght());
